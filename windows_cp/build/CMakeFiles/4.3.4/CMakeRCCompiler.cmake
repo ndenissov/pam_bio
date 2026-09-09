b2c8699e7d82c666924a87ae181f04acca026ab1
Nikita Denissov <n.denissov@proton.me>
2026-09-09 22:28:08 +0500

    chore: remove accidentally committed windows binaries and update .gitignore

--- windows_cp/build/CMakeFiles/4.3.4/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "/nix/store/1sgsv9jdhk2xjbwi520xw7vpw9njfh1p-x86_64-w64-mingw32-gcc-wrapper-15.3.0/bin/x86_64-w64-mingw32-windres")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .res)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
